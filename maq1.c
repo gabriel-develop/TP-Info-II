c685609e26b542e83ba8584b4446ea0279c79c0c
gabriel-develop <56320063+gabriel-develop@users.noreply.github.com>
2019-10-08 16:25:39 -0300

    Update maq1.c

--- maq1.c
+++ maq1.c
@@ -1,5 +1,12 @@
 // funciones.c
 
 int configurando(int e, char b) 
+
+printf("Bienvenido a mi programa  \n");
+delay(5000);
+printf("oops, no funciona  \n");
+printf("Hasta la proximaaaaaa");
+system("PAUSE");
+
 return 0;
 }
